c0a760107c8925ec52b1d383c388e95a1e4a72f4
Md Khairul Alom <khairul9215@gmail.com>
2023-09-21 22:08:57 +0600

    Final commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const imageEl = document.querySelector(".container");
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click",()=>{
+    imageNum = 6;
+    addNewImages();
+});
+
+function addNewImages(){
+    for (let index = 0; index < imageNum; index++) {
+    const newImgEl = document.createElement("img");
+    newImgEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*2000)}`
+    imageEl.appendChild(newImgEl);
+
+        
+    }
+    
+}
